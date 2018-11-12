5daf957dea30ed0dc43e68b5b308737328065265
pchauvel <pierre.chauvel@actency.fr>
2018-11-12 16:09:15 +0000

    avec le lien de menu

--- modules/custom/hello/src/Controller/HelloController.php
+++ modules/custom/hello/src/Controller/HelloController.php
@@ -7,7 +7,7 @@ use Drupal\Core\Controller\ControllerBase;
 class HelloController extends ControllerBase {
 	
 	public function content() {
-		$AccountName = \Drupal::currentUser()->getAccountName();
+		$AccountName = $this->currentUser()->getAccountName();
 		$markup = t('Welcome on Hello page. Your Account name is %name',[ '%name'=> $AccountName]);
 		
 		return ['#markup' => $markup];
