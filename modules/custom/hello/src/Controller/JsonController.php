311d5465990f029970ce21566d750d4479678580
pchauvel <pierre.chauvel@actency.fr>
2018-11-13 13:09:58 +0000

    JsonResponce

--- modules/custom/hello/src/Controller/JsonController.php
+++ modules/custom/hello/src/Controller/JsonController.php
@@ -3,15 +3,22 @@
 namespace Drupal\hello\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+
 
 class JsonController extends ControllerBase {
 	
 	public function content() {
+		
+		// Solution 1
 		$output = '{"json":"bien","ini":"pas bien"}';
 		$response = new Response();
 		$response->setContent($output);
 		$response->headers->set('Content-Type', 'application/json');
+		
+		// Solution 2
+		$response = new JsonResponse([1,2,3]);
 		
 		return $response;
 	}
